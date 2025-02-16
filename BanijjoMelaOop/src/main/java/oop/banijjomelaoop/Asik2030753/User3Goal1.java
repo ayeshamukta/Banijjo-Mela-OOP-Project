958374bf623115255b71eec9622f3023a1c4ed23
rifatashik <2030753@iub.edu.bd>
2025-02-16 19:31:51 +0600

    Added user3dasboard fxml ami maxwell Ashik

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal1.java
@@ -0,0 +1,30 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3Goal1
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TableView stallTableView;
+    @javafx.fxml.FXML
+    private TableColumn descriptionTableCol;
+    @javafx.fxml.FXML
+    private Button submitButtonOnClick;
+    @javafx.fxml.FXML
+    private TableColumn idtableTableCol;
+    @javafx.fxml.FXML
+    private RadioButton checkIssueStatusRadioButton;
+    @javafx.fxml.FXML
+    private TextArea descriptionTextArea;
+    @javafx.fxml.FXML
+    private TableColumn stallOwnerNametableCol;
+    @javafx.fxml.FXML
+    private TextField stallOwnerNameTextField;
+    @javafx.fxml.FXML
+    private TableColumn statusTableCol;
+    @javafx.fxml.FXML
+    private RadioButton reportNewIssueradioButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
