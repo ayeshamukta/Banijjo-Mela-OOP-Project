1fff767af7b011a8d9e2d470b0e95ceaa8ec40d6
rifatashik <2030753@iub.edu.bd>
2025-02-16 19:10:43 +0600

    Added user3dasboard fxml

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal1.java
@@ -0,0 +1,20 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3Goal1
+{
+    @javafx.fxml.FXML
+    private DatePicker deliveryDatePicker;
+    @javafx.fxml.FXML
+    private TextField customeerNameTextField;
+    @javafx.fxml.FXML
+    private TextField viewCustomerInfoTextField;
+    @javafx.fxml.FXML
+    private Button showDetailsButtonOnClick;
+    @javafx.fxml.FXML
+    private TextArea showTextArea;
+    @javafx.fxml.FXML
+    private TextField sendDeliveryTimetextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
