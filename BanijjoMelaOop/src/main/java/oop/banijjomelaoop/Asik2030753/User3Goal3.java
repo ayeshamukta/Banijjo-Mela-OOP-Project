35ab5434793d88e912c00dedafb8dd3c2133ca51
rifatashik <2030753@iub.edu.bd>
2025-02-19 21:27:46 +0600

    Added user3 goal2 and goal3  fxml ami maxwell Ashik

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal3.java
@@ -0,0 +1,24 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3Goal3
+{
+    @javafx.fxml.FXML
+    private ComboBox complaintsComboBox;
+    @javafx.fxml.FXML
+    private Button viewExsistingComplaintsButtonOnClick;
+    @javafx.fxml.FXML
+    private TextField stallIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn descriptionTableCol;
+    @javafx.fxml.FXML
+    private TableView goal3TableView;
+    @javafx.fxml.FXML
+    private TableColumn stallIdTableCol;
+    @javafx.fxml.FXML
+    private TextArea descriptionTextArea;
+    @javafx.fxml.FXML
+    private TableColumn complaintsTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3goal2.java
@@ -0,0 +1,31 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3goal2
+{
+    @javafx.fxml.FXML
+    private TextField stallIDTextField;
+    @javafx.fxml.FXML
+    private RadioButton viewExisistingComplaintsRadioButton;
+    @javafx.fxml.FXML
+    private TextField statusTextField;
+    @javafx.fxml.FXML
+    private TableColumn stallIdTableCol;
+    @javafx.fxml.FXML
+    private TextArea descriptionTextArea;
+    @javafx.fxml.FXML
+    private TableColumn complaintsTableCol;
+    @javafx.fxml.FXML
+    private RadioButton fileNewComplaintsRadioButton;
+    @javafx.fxml.FXML
+    private TableColumn descriptionTableaCol;
+    @javafx.fxml.FXML
+    private TableView goal2TableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtonOnClick(ActionEvent actionEvent) {
+    }
+}
