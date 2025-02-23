d7036ad910a659cf31a1665c6a799cb83a5cd085
rifatashik <2030753@iub.edu.bd>
2025-02-23 20:05:16 +0600

    Added user3goal 4 and 5 this is maxwell on duty ..Good bye

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal5.java
@@ -0,0 +1,27 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3Goal5
+{
+    @javafx.fxml.FXML
+    private TextField productDescriptionTextField;
+    @javafx.fxml.FXML
+    private TableColumn productDescriptionTableCol;
+    @javafx.fxml.FXML
+    private TextField productNameTextField;
+    @javafx.fxml.FXML
+    private TextField productTagsTextField;
+    @javafx.fxml.FXML
+    private TableColumn productNameTablecol;
+    @javafx.fxml.FXML
+    private TableColumn productTagsTableCol;
+    @javafx.fxml.FXML
+    private TableView ProductTableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtonOnClick(ActionEvent actionEvent) {
+    }
+}
