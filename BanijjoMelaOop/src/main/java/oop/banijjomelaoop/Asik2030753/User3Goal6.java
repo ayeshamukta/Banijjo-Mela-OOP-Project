6f14048cd1221c3342b893166625f2db58c36975
rifatashik <2030753@iub.edu.bd>
2025-02-25 11:21:21 +0600

    Added user3goal 7 this is maxwell on duty..Good bye

--- BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal6.java
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal6.java
@@ -3,5 +3,16 @@ package oop.banijjomelaoop.Asik2030753;
 public class User3Goal6
 {
     @javafx.fxml.FXML
+    private TextArea resultTextArea;
+    @javafx.fxml.FXML
+    private Button resultButtonOnClick;
+    @javafx.fxml.FXML
+    private ComboBox conditionComboBox;
+    @javafx.fxml.FXML
+    private TextField qualityTextField;
+    @javafx.fxml.FXML
+    private ComboBox quantityComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/Asik2030753/User3Goal7.java
@@ -0,0 +1,31 @@
+package oop.banijjomelaoop.Asik2030753;
+
+public class User3Goal7
+{
+    @javafx.fxml.FXML
+    private TableColumn deliveryLocationTableCol;
+    @javafx.fxml.FXML
+    private TableColumn logisticsNameTableCol;
+    @javafx.fxml.FXML
+    private TextField logisticsNamerTextField;
+    @javafx.fxml.FXML
+    private TextField travelTimeTextField;
+    @javafx.fxml.FXML
+    private ComboBox logisticsSupportComboBox;
+    @javafx.fxml.FXML
+    private TableColumn logisticsSupportTableCol;
+    @javafx.fxml.FXML
+    private TextField deliveryLocationTYextField;
+    @javafx.fxml.FXML
+    private TableView user3Goal7TableView;
+    @javafx.fxml.FXML
+    private TableColumn travelTimeTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void showButtonOnOnClick(ActionEvent actionEvent) {
+    }
+}
