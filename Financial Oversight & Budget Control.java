a9061697eed5197cf3207b81daa28a8403402233
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-18 01:01:07 +0600

    Create Financial Oversight & Budget Control.java
    Financial Oversight & Budget Control

---
+++ Financial Oversight & Budget Control.java
@@ -0,0 +1,8 @@
+public class AdminGoal8_BudgetControl {
+    public static void main(String[] args) {
+        System.out.println("Financial Oversight & Budget Control Section Accessed.");
+        System.out.println("→ Tracking stall rental, sponsorship, and ticket revenue...");
+        System.out.println("→ Ensuring budget allocation for infrastructure and logistics...");
+        System.out.println("Log Out Successful.");
+    }
+}
