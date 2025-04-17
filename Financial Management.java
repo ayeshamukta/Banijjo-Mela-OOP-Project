2fe5f7a822da8a6ef18c9ded0a55fcb0608eb554
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-18 00:56:58 +0600

    Create Financial Management.java

---
+++ Financial Management.java
@@ -0,0 +1,9 @@
+public class AdminGoal5_FinancialManagement {
+    public static void main(String[] args) {
+        System.out.println("Financial Management Dashboard Accessed.");
+        System.out.println("→ Viewing transactions and pending payments...");
+        System.out.println("→ Approving/Rejecting refunds...");
+        System.out.println("→ Generating financial summaries...");
+        System.out.println("Log Out Successful.");
+    }
+}
