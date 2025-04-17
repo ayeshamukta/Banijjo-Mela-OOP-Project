ce11b236f03e777901d67a479065f6fa2183175c
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-18 00:09:33 +0600

    Create Marketing and Promotions.java
    Creat Marketing and Promotions

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/mobin_1830071/Marketing and Promotions.java
@@ -0,0 +1,8 @@
+public class AdminGoal2_MarketingPromotions {
+    public static void main(String[] args) {
+        System.out.println("Marketing & Promotions Section Accessed.");
+        System.out.println("→ Launching digital & traditional advertising...");
+        System.out.println("→ Updating user database...");
+        System.out.println("Log Out Successful.");
+    }
+}
