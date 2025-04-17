9830de08af0da395bc57a01a1005a52c843a7032
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-18 01:06:31 +0600

    Create Visitor Traffic Analysis.java
    Visitor Traffic Analysis

---
+++ Visitor Traffic Analysis.java
@@ -0,0 +1,9 @@
+public class SecurityGoal4_VisitorTrafficAnalysis {
+    public static void main(String[] args) {
+        System.out.println("Accessed Visitor Traffic Analysis.");
+        System.out.println("→ Identifying rush hours and bottlenecks...");
+        System.out.println("→ Suggesting layout improvements...");
+        System.out.println("→ Generating peak hour flow optimization report...");
+        System.out.println("Log Out Successful.");
+    }
+}
