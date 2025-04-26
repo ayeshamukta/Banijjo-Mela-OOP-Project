5a95e45d424aeddb4570ec4d1cafb64d117ad2d0
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-26 23:05:11 +0600

    Create AdminGoal3ScheduleManagement.java

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/mobin_1830071/AdminGoal3ScheduleManagement.java
@@ -0,0 +1,9 @@
+public class AdminGoal3_ScheduleManagement {
+    public static void main(String[] args) {
+        System.out.println("Schedule Management Section Accessed.");
+        System.out.println("→ Viewing Event Schedule...");
+        System.out.println("→ Adding/Updating/Removing entries...");
+        System.out.println("→ Broadcasting updated schedule...");
+        System.out.println("Log Out Successful.");
+    }
+}
