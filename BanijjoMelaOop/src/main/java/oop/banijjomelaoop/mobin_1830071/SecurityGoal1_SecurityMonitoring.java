ca2ad6a1189b961ba3797dbd0d2da50ed866d052
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-26 23:15:14 +0600

    Create SecurityGoal1_SecurityMonitoring.java

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/mobin_1830071/SecurityGoal1_SecurityMonitoring.java
@@ -0,0 +1,9 @@
+public class SecurityGoal1_SecurityMonitoring {
+    public static void main(String[] args) {
+        System.out.println("Accessed Security Monitoring Section.");
+        System.out.println("→ Viewing real-time CCTV footage...");
+        System.out.println("→ Flagging and reporting suspicious activity...");
+        System.out.println("→ Notifying Admin about critical incidents...");
+        System.out.println("Log Out Successful.");
+    }
+}
