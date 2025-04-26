57c201c49a3ab3e3e322fb251fb0a4972f93afc8
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-26 23:19:18 +0600

    Create SecurityGoal5_PatrolManagement.java

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/mobin_1830071/SecurityGoal5_PatrolManagement.java
@@ -0,0 +1,9 @@
+public class SecurityGoal5_PatrolManagement {
+    public static void main(String[] args) {
+        System.out.println("Accessed Patrol Management System.");
+        System.out.println("→ Assigning patrol tasks to security team...");
+        System.out.println("→ Monitoring patrol updates in real-time...");
+        System.out.println("→ Receiving alerts on missed patrols or breaches...");
+        System.out.println("Log Out Successful.");
+    }
+}
