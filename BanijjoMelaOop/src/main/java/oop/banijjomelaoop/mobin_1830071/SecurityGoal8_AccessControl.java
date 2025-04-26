2f85269e1bf0ac3c2b3603d650a5c7c52efe2a3b
Md. Mobin Khan <1830071@iub.edu.bd>
2025-04-26 23:21:30 +0600

    Create SecurityGoal8_AccessControl.java

---
+++ BanijjoMelaOop/src/main/java/oop/banijjomelaoop/mobin_1830071/SecurityGoal8_AccessControl.java
@@ -0,0 +1,9 @@
+public class SecurityGoal8_AccessControl {
+    public static void main(String[] args) {
+        System.out.println("Accessed Access Control System.");
+        System.out.println("→ Verifying access to restricted areas...");
+        System.out.println("→ Approving or denying access permissions...");
+        System.out.println("→ Updating and generating access logs...");
+        System.out.println("Log Out Successful.");
+    }
+}
